077048f8bd2fc93c4187c8929e75499d9b3313bd
donellkemoli <donellkemoli@gmail.com>
2025-06-26 09:24:52 +0300

    added user controller

--- LendingWebApp/Data/LoanApplicationServiceDbContext.cs
+++ LendingWebApp/Data/LoanApplicationServiceDbContext.cs
@@ -5,13 +5,41 @@ namespace Loan_application_service.Data
 {
     public class LoanApplicationServiceDbContext : DbContext
     {
-        public LoanApplicationServiceDbContext (DbContextOptions<LoanApplicationServiceDbContext> options)
+        public LoanApplicationServiceDbContext(DbContextOptions<LoanApplicationServiceDbContext> options)
             : base(options)
         {
         }
 
-        public DbSet<LoanProduct> LoanProducts { get; set; } = default!;
+        public DbSet<Product> Products { get; set; } = default!;
+        public DbSet<Customer> Customers { get; set; } = default!;
+        public DbSet<LoanApplication> LoanApplications { get; set; } = default!;
+        public DbSet<Account> Accounts { get; set; } = default!;
         public DbSet<Users> Users { get; set; } = default!;
-        public DbSet<LoanApplication> LoanApplication { get; set; } = default!;
+        public DbSet<Notification> Notifications { get; set; } = default!;
+        public DbSet<AuditTrail> AuditTrails { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Account ↔ LoanApplication (1:1)
+            modelBuilder.Entity<Account>()
+                .HasOne(a => a.LoanApplication)
+                .WithOne(l => l.Account)
+                .HasForeignKey<Account>(a => a.ApplicationId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Account ↔ Customer (M:1)
+            modelBuilder.Entity<Account>()
+                .HasOne(a => a.Customer)
+                .WithMany()
+                .HasForeignKey(a => a.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // LoanApplication ↔ Customer (M:1)
+            modelBuilder.Entity<LoanApplication>()
+                .HasOne(l => l.Customer)
+                .WithMany()
+                .HasForeignKey(l => l.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
