10ea588ebe7a76149dd4cb76c975488ce82cdc8e
allanf90 <allannjoroge01m@gmail.com>
2025-07-04 22:55:52 +0300

    Adding the filters for soft delete for both loan poduct and loan charges classes.

--- LoanApplicationService.Core/Repository/LoanApplicationServiceDbContext.cs
+++ LoanApplicationService.Core/Repository/LoanApplicationServiceDbContext.cs
@@ -1,46 +1,68 @@
 ﻿using LoanApplicationService.Core.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System.Net.WebSockets;
 
-namespace LoanApplicationService.Core.Repository
+namespace LoanApplicationService.Core.Repository;
+
+public class LoanApplicationServiceDbContext(DbContextOptions<LoanApplicationServiceDbContext> options) : DbContext(options)
 {
-    public class LoanApplicationServiceDbContext(DbContextOptions<LoanApplicationServiceDbContext> options) : DbContext(options)
+    public DbSet<LoanProduct> LoanProducts { get; set; } = default!;
+    public DbSet<Users> Users { get; set; } = default!;
+    public DbSet<LoanApplication> LoanApplications { get; set; } = default!;
+    public DbSet<LoanCharge> LoanCharges { get; set; } = default!;
+    public DbSet<LoanChargeMapper> LoanChargeMapper { get; set; } = default!;
+
+    
+    public class LoanChargeMapConfiguration : IEntityTypeConfiguration<LoanChargeMapper>
     {
-        public DbSet<LoanProduct> LoanProducts { get; set; } = default!;
-        public DbSet<Users> Users { get; set; } = default!;
-        public DbSet<LoanApplication> LoanApplications { get; set; } = default!;
-        public DbSet<LoanCharge> LoanCharges { get; set; } = default!;
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        public void Configure(EntityTypeBuilder<LoanChargeMapper> builder)
         {
-            base.OnModelCreating(modelBuilder);
-
-            // Account to LoanApplication
-            modelBuilder.Entity<Account>()
-                .HasOne(a => a.LoanApplication)
-                .WithOne(l => l.Account)
-                .HasForeignKey<Account>(a => a.ApplicationId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            // Account to Customer
-            modelBuilder.Entity<Account>()
-                .HasOne(a => a.Customer)
-                .WithMany(c => c.Accounts)
-                .HasForeignKey(a => a.CustomerId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            // LoanApplication to Customer
-            modelBuilder.Entity<LoanApplication>()
-                .HasOne(l => l.Customer)
-                .WithMany(c => c.LoanApplications)
-                .HasForeignKey(l => l.CustomerId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            //loanproduct to loancharge
-
-            modelBuilder.Entity<LoanCharge>();
-
+            builder.HasKey(s => new { s.LoanChargeId, s.LoanProductId });
+            builder.HasOne(ss => ss.LoanProduct)
+                .WithMany(s => s.LoanChargeMap)
+                .HasForeignKey(ss => ss.LoanProductId);
+            builder.HasOne(ss => ss.LoanCharge)
+                .WithMany(s => s.LoanChargeMap)
+                .HasForeignKey(ss => ss.LoanChargeId);
         }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Account to LoanApplication
+        modelBuilder.Entity<Account>()
+            .HasOne(a => a.LoanApplication)
+            .WithOne(l => l.Account)
+            .HasForeignKey<Account>(a => a.ApplicationId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Account to Customer
+        modelBuilder.Entity<Account>()
+            .HasOne(a => a.Customer)
+            .WithMany(c => c.Accounts)
+            .HasForeignKey(a => a.CustomerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // LoanApplication to Customer
+        modelBuilder.Entity<LoanApplication>()
+            .HasOne(l => l.Customer)
+            .WithMany(c => c.LoanApplications)
+            .HasForeignKey(l => l.CustomerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.ApplyConfiguration(new LoanChargeMapConfiguration());
+
+        // Global filters for soft delete
+        modelBuilder.Entity<LoanProduct>().Property(p => p.IsDeleted).HasDefaultValue(false);
+        modelBuilder.Entity<LoanProduct>().HasQueryFilter(p => !p.IsDeleted);
+
+        modelBuilder.Entity<LoanCharge>().Property(p => p.IsDeleted).HasDefaultValue(false);
+        modelBuilder.Entity<LoanCharge>().HasQueryFilter(p => !p.IsDeleted);
 
     }
+
+
+
 }
