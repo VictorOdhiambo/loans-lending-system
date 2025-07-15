e79b8ce965ebc37223cd560e9dabbac97345bca7
allanf90 <allannjoroge01m@gmail.com>
2025-07-15 12:07:15 +0300

    Adding the loan charge mapper dto class.

---
+++ LoanApplicationService.Service/DTOs/LoanModule/LoanChargeMapperDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanApplicationService.Service.DTOs.LoanModule
+{
+    public class LoanChargeMapperDto
+    {
+        public int LoanProductId { get; set; }
+        public int LoanChargeId { get; set; }
+        
+    }
+}
