b36a2b74f9c0d2c1b51ab490d98b2908e3158735
allanf90 <allannjoroge01m@gmail.com>
2025-07-21 15:58:22 +0300

    Loan withdrawal model and Dto classes

---
+++ LoanApplicationService.Core/Models/LoanWithdrawal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanApplicationService.Core.Models
+{
+    public class LoanWithdrawal
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int WithdrawalId { get; set; }
+        public int AccountId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime WithdrawalDate { get; set; }
+        public string Status { get; set; } = "Pending"; 
+        public int PaymentMethodId { get; set; } 
+        public virtual Account Account { get; set; }
+    }
+}
---
+++ LoanApplicationService.Service/DTOs/LoanWithdrawal/LoanWithdawalDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanApplicationService.Service.DTOs.LoanDisbursement
+{
+    public class LoanWithdawalDto
+    {
+        public int AccountId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DisbursementDate { get; set; }
+        public string Status { get; set; } = "Pending";
+        public int PaymentMethod { get; set; }
+    }
+}
