3ec1dcfcdd73d4d2c4b62bf92d456c403722c609
allanf90 <allannjoroge01m@gmail.com>
2025-07-21 15:58:49 +0300

    Loan Disbursement mapper

---
+++ LoanApplicationService.Service/Mapper/LoanDisbursementMapper/LoanDisbursementProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using LoanApplicationService.Core.Models;
+using LoanApplicationService.Service.DTOs.LoanDisbursement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanApplicationService.Service.Mapper.LoanDisbursementMapper
+{
+    public class LoanDisbursementProfile :Profile
+    {
+        public LoanDisbursementProfile()
+        {
+            CreateMap<LoanWithdawalDto, LoanWithdrawal>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status ?? "Pending"));
+            CreateMap<LoanWithdrawal, LoanWithdawalDto>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+        }
+    }
+}
